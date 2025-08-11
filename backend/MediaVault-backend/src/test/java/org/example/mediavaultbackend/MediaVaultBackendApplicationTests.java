f3f3e0b2fa40a53dc836015264167f15de7a3c02
Julian <cy21ujid@studserv.uni-leipzig.de>
2025-08-11 16:49:01 +0200

    added backend + docker compose

---
+++ backend/MediaVault-backend/src/test/java/org/example/mediavaultbackend/MediaVaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mediavaultbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaVaultBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
